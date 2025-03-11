be200644072edd32a754214d18c13349fe843ef1
Agustin Krapovickas <agukrapo@gmail.com>
2025-03-11 01:04:51 -0300

    refactor: linter fixes

--- e2e/container_test.go
+++ e2e/container_test.go
@@ -1,7 +1,6 @@
 package e2e
 
 import (
-	"context"
 	"fmt"
 	"io"
 	"net/http"
@@ -15,27 +14,26 @@ import (
 )
 
 func Test(t *testing.T) {
-	ctx := context.Background()
 	contReq := testcontainers.ContainerRequest{
 		Image:        "simpler-mock-server:latest",
 		ExposedPorts: []string{"4321/tcp"},
 		WaitingFor:   wait.ForLog("Server started on :4321"),
 	}
-	sms, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+	sms, err := testcontainers.GenericContainer(t.Context(), testcontainers.GenericContainerRequest{
 		ContainerRequest: contReq,
 		Started:          true,
 	})
 	require.NoError(t, err)
 	defer func() {
-		require.NoError(t, sms.Terminate(ctx))
+		require.NoError(t, sms.Terminate(t.Context()))
 	}()
 
-	endpoint, err := sms.Endpoint(ctx, "")
+	endpoint, err := sms.Endpoint(t.Context(), "")
 	require.NoError(t, err)
 
 	t.Run("DELETE", func(t *testing.T) {
 		url := fmt.Sprintf("http://%s/api/people/a3b69b44-d562-11eb-b8bc-0242ac130003", endpoint)
-		req, err := requests.New(url).Method(http.MethodDelete).Build(ctx)
+		req, err := requests.New(url).Method(http.MethodDelete).Build(t.Context())
 		require.NoError(t, err)
 
 		res, err := http.DefaultClient.Do(req)
@@ -52,7 +50,7 @@ func Test(t *testing.T) {
 
 	t.Run("GET", func(t *testing.T) {
 		url := fmt.Sprintf("http://%s/health", endpoint)
-		req, err := requests.New(url).Build(ctx)
+		req, err := requests.New(url).Build(t.Context())
 		require.NoError(t, err)
 
 		res, err := http.DefaultClient.Do(req)
@@ -69,7 +67,7 @@ func Test(t *testing.T) {
 
 	t.Run("PATCH", func(t *testing.T) {
 		url := fmt.Sprintf("http://%s/api/people/a3b69b44-d562-11eb-b8bc-0242ac130003", endpoint)
-		req, err := requests.New(url).Method(http.MethodPatch).Build(ctx)
+		req, err := requests.New(url).Method(http.MethodPatch).Build(t.Context())
 		require.NoError(t, err)
 
 		res, err := http.DefaultClient.Do(req)
@@ -86,7 +84,7 @@ func Test(t *testing.T) {
 
 	t.Run("POST", func(t *testing.T) {
 		url := fmt.Sprintf("http://%s/api/people/a3b69b44-d562-11eb-b8bc-0242ac130003", endpoint)
-		req, err := requests.New(url).Method(http.MethodPost).Build(ctx)
+		req, err := requests.New(url).Method(http.MethodPost).Build(t.Context())
 		require.NoError(t, err)
 
 		res, err := http.DefaultClient.Do(req)
@@ -98,12 +96,12 @@ func Test(t *testing.T) {
 
 		assert.Equal(t, http.StatusTeapot, res.StatusCode)
 		assert.Equal(t, "application/json", res.Header.Get("Content-Type"))
-		assert.Equal(t, "{\n  \"people\": \"418 I'm a teapot\"\n}", string(b))
+		assert.JSONEq(t, "{\n  \"people\": \"418 I'm a teapot\"\n}", string(b))
 	})
 
 	t.Run("PUT", func(t *testing.T) {
 		url := fmt.Sprintf("http://%s/api/people", endpoint)
-		req, err := requests.New(url).Method(http.MethodPut).Build(ctx)
+		req, err := requests.New(url).Method(http.MethodPut).Build(t.Context())
 		require.NoError(t, err)
 
 		res, err := http.DefaultClient.Do(req)
@@ -120,7 +118,7 @@ func Test(t *testing.T) {
 
 	t.Run("create route after not found", func(t *testing.T) {
 		url := fmt.Sprintf("http://%s/qwerty", endpoint)
-		req, err := requests.New(url).Build(ctx)
+		req, err := requests.New(url).Build(t.Context())
 		require.NoError(t, err)
 
 		res, err := http.DefaultClient.Do(req)
